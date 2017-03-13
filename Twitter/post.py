64c00eba5d1e3f1b28e4d9bbdfbebb3671ce3157
capfr <victorr.limaa@hotmail.com>
2017-03-13 14:24:14 -0300

    q

--- Twitter/post.py
+++ Twitter/post.py
@@ -3,3 +3,7 @@ import tweepy
 
 auth = tweepy.OAuthHandler(Connection.consumer_key, Connection.consumer_secret)
 auth.set_access_token(Connection.token_key,Connection.token_secret)
+
+api = tweepy.API(auth)
+
+api.update_with_media('1.jpg','teste')
