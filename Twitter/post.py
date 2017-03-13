8233a4ab4c4b8282a38a7e2e6629a6fb8f219e5f
capfr <victorr.limaa@hotmail.com>
2017-03-13 14:06:19 -0300

    teste

--- Twitter/post.py
+++ Twitter/post.py
@@ -1,6 +1,9 @@
 from connection import Connection
-from twitter import *
-twitter = Twitter(
-      auth=OAuth(Connection.token_key, Connection.token_secret, Connection.consumer_key, Connection.consumer_secret))
-twitter.statuses.update(
-    status="alo")
+import tweepy
+
+auth = tweepy.OAuthHandler(Connection.consumer_key, Connection.consumer_secret)
+auth.set_access_token(Connection.token_key,Connection.token_secret)
+
+api = tweepy.API(auth)
+
+api.update_with_media('1.jpg','teste')
